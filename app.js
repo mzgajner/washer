db954e70d9cfe5a76e1a6baeab5c060abd959a35
mzgajner <mato@zgajner.com>
2017-08-20 12:08:50 +0200

    Replace linter config and lint files

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
-const http = require('http')
-const url = require('url')
-const finalhandler = require('finalhandler')
-const serveStatic = require('serve-static')
-const sendNotification = require('./sendNotification')
+const http = require('http');
+const url = require('url');
+const finalhandler = require('finalhandler');
+const serveStatic = require('serve-static');
+const sendNotification = require('./sendNotification');
 
-const serve = serveStatic('./client')
-const registrationIds = []
+const serve = serveStatic('./client');
+const registrationIds = [];
 
 const server = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url, true)
+  const parsedUrl = url.parse(req.url, true);
 
   if (parsedUrl.pathname === '/api/send') {
-    sendNotification(registrationIds)
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('notification sent')
-  }
-  else if (parsedUrl.pathname === '/api/register') {
+    sendNotification(registrationIds);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('notification sent');
+  } else if (parsedUrl.pathname === '/api/register') {
     if (registrationIds.indexOf(parsedUrl.query.id) === -1) {
-      registrationIds.push(parsedUrl.query.id)
+      registrationIds.push(parsedUrl.query.id);
     }
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('id registered')
-    console.log('registered', registrationIds)
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('id registered');
+    console.log('registered', registrationIds);
+  } else {
+    const done = finalhandler(req, res);
+    serve(req, res, done);
   }
-  else {
-    const done = finalhandler(req, res)
-    serve(req, res, done)
-  }
-})
-server.listen(process.env.PORT)
+});
+server.listen(process.env.PORT);
--- sendNotification.js
+++ sendNotification.js
@@ -1,27 +1,27 @@
-const http = require('http')
+const http = require('http');
 
-module.exports = (registration_ids) => {
+module.exports = (registrationIds) => {
   const postData = JSON.stringify({
-    registration_ids : registration_ids
-  })
+    registration_ids: registrationIds,
+  });
 
   const options = {
-    hostname : 'android.googleapis.com',
-    path     : '/gcm/send',
-    method   : 'POST',
-    headers  : {
-      'Content-Type'  : 'application/json',
-      'Authorization' : `key=${process.env.API_KEY}`,
-      'Content-Length': Buffer.byteLength(postData)
-    }
-  }
+    hostname: 'android.googleapis.com',
+    path: '/gcm/send',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `key=${process.env.API_KEY}`,
+      'Content-Length': Buffer.byteLength(postData),
+    },
+  };
 
-  const request = http.request(options)
+  const request = http.request(options);
 
-  request.on('error', e => {
+  request.on('error', (e) => {
     throw new Error(`problem with request: ${e.message}`);
-  })
+  });
 
-  request.write(postData)
-  request.end()
-}
+  request.write(postData);
+  request.end();
+};
